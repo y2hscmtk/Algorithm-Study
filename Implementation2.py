200f167d3f2581537513c1546cde575cec056816
Y2K <y2_12@naver.com>
2022-08-14 20:26:26 +0900

    idea1_implement2

--- Implementation2.py
+++ Implementation2.py
@@ -14,5 +14,20 @@
 # 입력 조건 : 첫째 줄에 정수 N이 입력된다.
 # 출력 조건 : 00시 00분 00초부터 N시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든 경우의 수를 출력한다.
 
-n = int(input())
+from http.client import CannotSendRequest
 
+
+n = int(input())
+count = 0
+time = list(0,0,0) #시, 분, 초
+check3 = list(3,13,23,33,43,53)
+
+for i in range(n): #0시부터 작동 시작
+    for s in range(60):
+        time[2]+=1 # 1초씩 증가시킴
+        for c in check3:
+            if time[2]==c:
+                count+=1 #3이 하나라도 들어가는 경우에 대해, 조건 하나 증가시키기
+                
+
+print(count)
