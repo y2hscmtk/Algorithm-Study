dfa620d44abd45ed999d6bfc93ea63bc88dff1f5
Y2K <y2_12@naver.com>
2022-08-26 23:41:24 +0900

    선택정렬 수행시, 튜플의 값은 변경되지않으므로 이상한 정렬이 수행됨

--- sort2.py
+++ sort2.py
@@ -22,15 +22,33 @@ for i in range(n):
     
 # 리스트안에 튜플로서 자료를 보관한 형태?
 
-# 낮은 성적순으로 자료 정리하기? => 오름차순으로 자료 정리 => 튜플은 정렬,스왑,등이 되지않는 자료구조임을 유의
-for i in range(n):
-    min_index = i
-    for j in range(i+1,n): #i+1번째 인덱스부터 n-1번째 인덱스까지 반복하여 최소값이 있는 인덱스 번호를 파악
-        if array[min_index][1]>array[j][1]: #더 낮은 성적이 존재한다면
-            min_index = j #j번째 인덱스를 가장 낮은 인덱스로 설정 => 반복
-    array[i][1],array[min_index][1] = array[min_index][1],array[i][1] # 스왑을 통하여 가장 낮은 성적이 가장 처음에 오도록 변경
+# # 낮은 성적순으로 자료 정리하기? => 오름차순으로 자료 정리 => 튜플은 정렬,스왑,등이 되지않는 자료구조임을 유의
+# for i in range(n):
+#     min_index = i
+#     for j in range(i+1,n): #i+1번째 인덱스부터 n-1번째 인덱스까지 반복하여 최소값이 있는 인덱스 번호를 파악
+#         if array[min_index][1]>array[j][1]: #더 낮은 성적이 존재한다면
+#             min_index = j #j번째 인덱스를 가장 낮은 인덱스로 설정 => 반복
+#     array[i][1],array[min_index][1] = array[min_index][1],array[i][1] # 스왑을 통하여 가장 낮은 성적이 가장 처음에 오도록 변경
 
-# 정렬이후
 
+# # 정렬이후
+# for i in range(n):
+#     print(array[i][0], end=' ')
+
+# 이름을 저장할 새로운 리스트 생성
+name_array = []
+# 입력받은 튜플리스트에서 이름만 따로 빼어 저장
 for i in range(n):
-    print(array[i][0], end=' ')
+    name_array.append(array[i][0])
+    
+# 선택정렬 => 새로운 리스트를 만들어서 성적순에 따라 새로운 리스트의 이름 조작
+for i in range(n):
+    min_index = i #첫번째 인덱스가 가장 낮은 성적을 가진 인덱스라 가정
+    for j in range(i+1,n): #i+1번째 수중에서 i번째 인덱스보다 낮은 값을 가진 인덱스 탐색
+        if array[min_index][1]>array[j][1]: #j번째 인덱스가 더 낮은 성적을 가진 인덱스라면?
+            min_data = j
+    name_array[i],name_array[min_data] = name_array[min_data],name_array[i]
+
+# 정렬 이후 낮은 성적순으로 정렬된 이름 리스트 출력
+for i in name_array:
+    print(i)
