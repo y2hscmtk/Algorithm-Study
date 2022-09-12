cc7290da04db8a8a79e72b813e44ac184a29b5da
Y2K <y2_12@naver.com>
2022-09-13 00:55:11 +0900

    깃허브 테스트

--- 1339.py
+++ 1339.py
@@ -7,59 +7,20 @@
 # N개의 단어가 주어졌을 때, 그 수의 합을 최대로 만드는 프로그램을 작성하시오.
 
 # 아이디어 : 같은 자리수가 되기 전까지 9부터 차례로 값을 대입, 같은 자리수부터는 단어의 길이가 긴 단어부터 차례로 값을 대입한다.
+# => 실패
 
+# 아이디어2 : 수학으로 접근
 
-n = int(input())  # 단어의 개수 입력받기
+# BCD
+# ABC
+# => 100B + 10C + D + 100A + 10B + C => 110B + 100A + 11C + D
+n = int(input())
 
-word = []  # 입력받은 단어를 저장할 리스트
-
-word_data = [-1]*26  # 값을 할당받지 못한 문자는 -1으로 할당한다.
-
-max_sum = 0  # 최대값을 저장하기 위한 변수
-number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-# 단어를 입력받는 단계
+word = []
 for i in range(n):
-    word.append(list(input()))  # 각각의 단어를 단어별로 구별하기 위해 리스트 형태로 입력받는다.
+    word.append(input())
 
-word.sort()  # 먼저 알파벳 순으로 정렬을 한다.
+word_data = [-1]*26
 
-# 단어의 글자 길이 순으로 변경하는 과정
-for i in range(n):
-    for j in range(1, n):
-        if len(word[j-1]) < len(word[j]):
-            word[j-1], word[j] = word[j], word[j-1]  # 위치 변경
-
-size = 9
-# print(ord('A')-65) = 0 # ord() 아스키코드 변환 함수
-for i in range(1, n):
-    # 두번째로 길이가 긴 단어의 길이전까지 값을 대입한다.
-    for j in range(len(word[i-1])-len(word[i])):
-        # 아직 값을 할당 받지 못한 경우 해당 알파벳의 위치에 값을 대입해준다.
-        if word_data[ord(word[i-1][j])-65] == -1:
-            word_data[ord(word[i-1][j])-65] = size
-            word[i-1][j] = size
-            size -= 1  # 다음 단어는 size--인 값을 대입해준다.
-        else:
-            # 이미 단어 데이터에 값이 할당되어 있다면, 그 값을 불러와 할당시킨다.
-            word[i-1][j] = word_data[ord(word[i-1][j])-65]
-
-# 값을 할당받지 못한 나머지 알파벳에 대해 값을 할당하는 과정
-start = len(word[0])-len(word[1])
-for i in range(1, n):
-    # if word[i][k] not in number:
-
-    # for j in range(start, len(word[i-1])):
-    if word[i-1][start] not in number:
-        # 단어 데이터에 해당 단어가 있는지 확인
-        if word_data[ord(word[i-1][start])-65] != -1:  # -1이 아니라면? => 이미 값이 할당되어 있다면?\
-            word[i -
-                 1][start] = word_data[ord(word[i-1][start])-65]  # 할당되어있는값으로 변환
-        else:
-            word_data[ord(word[i-1][start])-65] = size
-            word[i-1][start] = size
-            size -= 1
-
-
-print(word)
-# print(max_sum)
+test = 1
+test = 2
