91d94b15b45378473fc9b8d1136efbb95408b4c5
Y2K <y2_12@naver.com>
2022-09-14 00:05:03 +0900

    첫번째 시도

--- 1049.py
+++ 1049.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/1049
+
 n, m = map(int, input().split())
 
 six_set = []  # 6개들이를 살때의 가격
@@ -7,7 +9,23 @@ min_price = 0  # 가격의 최소값
 for i in range(m):
     data1, data2 = map(int, input().split())
     six_set.append(data1)
-    one_set.appned(data2)
+    one_set.append(data2)
 
 six_set.sort()  # 오름차순 정렬
 one_set.sort()  # 내림차순 정렬
+
+if n >= 6:
+    if n % 6 == 0:  # 6개의 배수개일때
+        min_price = six_set[0]*n//6
+    else:
+        min_price = six_set[0]*(n//6) + one_set[0]*(n % 6)
+    if six_set[0]*(n//6) + six_set[0] < min_price:
+        min_price = six_set[0]*(n//6) + six_set[0]
+elif n < 6:
+    # 1개 들이로만 살때와 6개들이로 사서 양을 초과할때를 서로 비교
+    if one_set[0]*n < six_set[0]:
+        min_price = one_set[0]*n
+    else:
+        min_price = six_set[0]  # 초과해서 살때가 더 쌀수 있음
+
+print(min_price)
