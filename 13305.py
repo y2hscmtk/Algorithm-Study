56b602648237e2f90cec2188fc076268a7458c89
Y2K <y2_12@naver.com>
2022-09-16 22:20:32 +0900

    백준 13305번 풀이: 부분정답

--- 13305.py
+++ 13305.py
@@ -9,6 +9,19 @@
 
 n = int(input())  # 도시의 개수
 
-d = [list(map(int, input().split()))]  # 도시간에 서로 떨어진 km수를 저장할 배열
+d = list(map(int, input().split()))  # 도시간에 서로 떨어진 km수를 저장할 배열
 
-city_oil = [list(map(int, input().split()))]  # 각 도시의 기름 값을 저장할 배열
+city_oil = list(map(int, input().split()))  # 각 도시의 기름 값을 저장할 배열
+
+price = city_oil[0]*d[0]  # 최소 기름값을 저장할 변수
+
+# 최소 기름값 탐색 시작
+for i in range(1, n-1):  # 마지막 도시는 고려할 대상이 아니므로 n-1번 비교
+    # if i >= 1:  # 두번째 도시부터 값 비교 시작
+    #     # 이전도시에서 다음도시까지 기름을 모두 샀을때와 비교하여 더 작은지 판별
+    if city_oil[i-1]*(d[i-1]+d[i]) < price + city_oil[i]*d[i]:
+        price += (city_oil[i-1]*d[i])  # 이전도시에서 기름을 구매했을때가 더 이득인 경우
+    else:
+        price += city_oil[i]*d[i]
+
+print(price)
