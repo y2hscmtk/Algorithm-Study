8185a61a3151af10faee3c491d72fc9494b4933f
Y2K <y2_12@naver.com>
2022-09-30 23:47:21 +0900

    1744번 중간 저장

--- 1744.py
+++ 1744.py
@@ -11,3 +11,76 @@
 # 수열의 모든 수는 단 한번만 묶거나, 아니면 묶지 않아야한다.
 
 # # 수열이 주어졌을 때, 수열의 각 수를 적절히 묶었을 때, 그 합이 최대가 되게 하는 프로그램을 작성하시오.
+
+'''
+백준 1744번 재도전
+
+생각가능한 시나리오는 다음과 같다.
+1. 양수인 경우
+2. 음수인 경우
+3. 0인 경우
+
+양수인 경우에 대해서, 
+1인 경우는 다른 수와 곱하지 않고 더한다.
+양수는 큰수끼리 곱하면 더 커진다.
+음수인 경우에 대해서, 음수는 음수끼리 곱하면 양수가 되고, 이 경우에는 큰 음수끼리 곱하고 남은 수를 더하는것이 이득이다.
+(-1,-2,-3) => 6-1 = 5 (max)
+
+0인 경우에 대해서,
+음수가 1개 있을경우, 해당 음수와 곱해서 음수를 제거하는것이 이득이다.
+음수가 2n개 있을경우, 음수끼리 곱하면 양수가 되므로 배열에서 0을 제거하는것이 이득이다.
+음수가 2n+1개 있을경우, 음수들 중, 가장 큰값(-1)과 곱해서 제거하는것이 이득이다.(나머지 2개의 음수끼리 곱하면 되기때문이다.)
+음수가 없는 경우는 배열에서 제거하는것이 이득이다.
+
+다음 상황에 맞게 그리디 알고리즘을 적용시키면 문제 해결이 가능할것으로 보인다.
+'''
+import re
+from turtle import pos
+
+
+n = int(input())
+zero = []  # 0을 저장할 배열
+one = []
+possitive_array = []  # 양수를 저장할 배열
+negative_array = []  # 음수를 저장할 배열
+max_sum = 0
+
+for _ in range(n):
+    number = int(input())
+    if number > 1:
+        possitive_array.append(number)
+    elif number < 1:
+        negative_array.append(number)
+    elif number == 0:
+        zero.append(number)
+    else:
+        one.append(number)
+
+possitive_array.sort()  # 오름차순
+negative_array.sort(reverse=True)  # 내림차순 -1, -2, -3 ...
+
+# 최대합 구하는 과정
+
+# 0단계: 1은 어떤수와도 연관되지 않고 그냥 더한다.
+max_sum = sum(one)
+
+# 1단계: 양수 배열에서 2개씩 곱해가며 더한다.
+for i in range(0, len(possitive_array), 2):
+    if i+1 < len(possitive_array):
+        # 가장 큰수와 두번째로 큰수를 곱해서 더한다.
+        max_sum += (possitive_array[i]*possitive_array[i+1])
+    else:
+        # 양수가 홀수개 있을경우 홀로 남은 수는 더한다.
+        max_sum += possitive_array[i]
+
+# 2단계: 0처리
+if len(zero) != 0:
+    # 0은 음수와 곱하는 과정을 통해 음수를 제거할수 있다.
+    # 음수가 짝수개 일 경우 음수끼리 곱해서 양수를 만드는것이 이득이므로
+    # 0은 모두 무시한다.
+    if len(negative_array) % 2 != 0:  # 음수가 홀수개인 경우만 해당 계산 진행
+        for i in range(len(zero)):
+            del zero(i)  # 음수의 개수만큼
+
+# 3단계: 음수 처리
+for i in range(0, len(negative_array), 2):
