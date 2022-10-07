f307745bd5250e476c83a868f52f7085f3c13fdd
Y2K <y2_12@naver.com>
2022-10-08 00:49:41 +0900

    1260번 11프로 오답

--- 1260.py
+++ 1260.py
@@ -14,23 +14,58 @@
 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
 '''
 
-# n은 정점의 개수, m은 간선의 개수, v는 탐색을 시작할 정점의 번호(시작정점)
+from collections import deque
 n, m, v = map(int, input().split())
 
 vertix = [i for i in range(1, n+1)]  # 정점의 정보를 저장할 리스트
-edge = []
 
+edge = []
 for _ in range(m):
     edge.append(list(map(int, input().split())))
 
-edge.sort()  # 오름차순으로 정렬 1번 정점부터 연결정보가 표시될것
+# edge.sort()  # 오름차순으로 정렬 1번 정점부터 연결정보가 표시될것 # 굳이 오름차순 할 필요 없을듯
 
-adj = [[] for v in vertix]  # 정점의 개수만큼 생성
+adj = [[] for vt in vertix]  # 정점의 개수만큼 생성
 
 # 연결리스트 생성
 for e in edge:  # 간선 리스트에서 간성정보를 한개씩 뽑아서
     adj[e[0]-1].append(e[1])  # 간선 삽입 0번 인덱스부터 삽입되어야 함
     adj[e[1]-1].append(e[0])  # 위 문제의 경우 양방향 리스트임
 
-
 # 첫째줄에는 DFS를 수행한 결과 두번째 줄에는 BFS를 수행한 결과를 출력한다.
+
+# DFS 알고리즘 : 깊이 우선 탐색
+# 미로 찾기라고 생각 => 미로를 찾는것처럼 한쪽으로 갈수 있는 만큼 가다가 더이상 갈수 없게되면 가장 가까운 갈림길로 되돌아간다.
+# 되돌아가야 되기때문에, 돌아갈 곳의 위치를 저장할 스택이 필요함 => 재귀호출을 통해 묵시적 스택사용이 가능하다.
+
+stack = [v]  # 시작정점은 v임
+dfs_visited = []  # 방문한 정점을 저장할 리시트
+while stack:
+    c = stack.pop()  # 현재위치를 의미
+    dfs_visited.append(c)
+    for neighbor in adj[c-1]:
+        if neighbor not in dfs_visited:
+            stack.append(neighbor)
+            break
+
+# BFS 알고리즘 : 너비 우선 탐색
+# 가까이에 있는 정점을 먼저 방문하고, 멀리 떨어져 있는 정점을 나중에 방문한다.
+# "큐"를 사용하여 구현한다.
+
+bfs_visited = []
+queue = deque()
+
+queue.append(v)
+while queue:
+    c = queue.popleft()
+    bfs_visited.append(c)
+    for neighbor in adj[c-1]:
+        if neighbor not in bfs_visited and neighbor not in queue:
+            queue.append(neighbor)
+
+
+for i in dfs_visited:
+    print(i, end=' ')
+print()
+for i in bfs_visited:
+    print(i, end=' ')
