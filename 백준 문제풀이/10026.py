d1a6cd4aba4a6ae5fea23a3edf935418c0a7ea19
Y2K <y2_12@naver.com>
2022-11-10 21:03:06 +0900

    백준 10026번 풀이

--- 백준 문제풀이/10026.py
+++ 백준 문제풀이/10026.py
@@ -28,3 +28,53 @@ RRRRR
 RGB와 차별성을 두기 위해 방문한 정점은 X라는 키워드로 값을 변경시키고 적록색약인 사람이 탐색한 결과도 표현해야하므로
 깊은 복사를 통해 배열을 복사해서 진행한다.
 '''
+
+
+from collections import deque
+import copy
+n = int(input())
+
+
+def append_queue(queue, start, end):
+    queue.append([start+1, end])
+    queue.append([start-1, end])
+    queue.append([start, end+1])
+    queue.append([start, end-1])
+
+
+def bfs(graph, start, end):
+    global n
+    color = graph[start][end]  # 색 정보 저장
+    graph[start][end] = 'X'  # 방문처리
+    queue = deque()
+    append_queue(queue, start, end)  # 인접 정점 삽입
+    while queue:
+        data = queue.popleft()
+        s, e = data[0], data[1]
+        if 0 <= s < n and 0 <= e < n:  # 영역을 벗어나지 않고,
+            if graph[s][e] == color:  # 같은 색 영역이라면
+                graph[s][e] = 'X'  # 방문처리
+                append_queue(queue, s, e)
+
+
+graph = [list(input()) for _ in range(n)]
+
+graph_abnormal = copy.deepcopy(graph)  # 적록색약인 사람이 이용할 그래프
+
+for i in range(n):
+    for j in range(n):
+        if graph_abnormal[i][j] == 'G':
+            graph_abnormal[i][j] = 'R'  # R과 G를 구별하지 못하므로 같은 문자로 변경한다.
+
+count = 0
+count2 = 0
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 'X':  # 방문하지 않은 정점이라면 => 새로운 그룹이라면
+            count += 1
+            bfs(graph, i, j)
+        if graph_abnormal[i][j] != 'X':
+            count2 += 1
+            bfs(graph_abnormal, i, j)
+print(count, count2)
