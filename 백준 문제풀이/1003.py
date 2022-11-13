670a7552bf69e281e6dd4cb81c20168695db1827
Y2K <y2_12@naver.com>
2022-11-13 23:58:08 +0900

    다이나믹 프로그래밍 기법으로 다시 풀것

--- 백준 문제풀이/1003.py
+++ 백준 문제풀이/1003.py
@@ -2,11 +2,21 @@
 
 # 피보나치 함수
 def fibonacci(n):
+    global count0, count1
     if n == 0:
-        print("0")
+        count0 += 1
         return 0
     elif n == 1:
-        print("1")
+        count1 += 1
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+
+
+count0, count1 = 0, 0  # fibo(0)과 fibo(1)의 출력횟수를 카운트한다.
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    fibonacci(n)
+    print(count0, count1)
+    count0, count1 = 0, 0
