90777d110b91a606e825149791a9a36d0dfb7369
Y2K <y2_12@naver.com>
2022-11-15 00:14:27 +0900

    수가 매우 많을때는 퀵 정렬,등을 사용하는것이 좋음

--- 백준 문제풀이/2751.py
+++ 백준 문제풀이/2751.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/2751
+
+# 선택 정렬 방식으로 풀어보기
+
+n = int(input())
+
+list = []
+for _ in range(n):
+    list.append(int(input()))
+
+# 여기서부터 정렬 시작
+# 오름차순으로 정렬시키는것이 목표
+# 선택 정렬 => swap을 이용해서 오른쪽 리스트의 가장 큰값과 인덱스 교환
+for i in range(n):
+    large = i  # 0번째 부터 비교
+    for j in range(i+1, n, 1):  # 1번째 인덱스부터 마지막 인덱스까지 중
+        if list[j] > list[large]:  #
+            large = j  # 가장 큰 값의 인덱스 업데이트
+        list[j], list[large] = list[large], list[j]
+
+
+for i in list:
+    print(i)
