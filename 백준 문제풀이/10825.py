71b531337d2b93e93ddd62d1d2c2c59581424c34
Y2K <y2_12@naver.com>
2022-11-30 19:07:09 +0900

    백준 10825번 풀이 : sort()라이브러리의 lambda에 대한 이해

--- 백준 문제풀이/10825.py
+++ 백준 문제풀이/10825.py
@@ -12,6 +12,21 @@
 num = int(input())
 
 
-data = []
+# 데이터 입력받기 # 우선 모두 문자형으로 데이터를 입력받는다.
+data = [list(input().split()) for _ in range(num)]
 
-for _ in range(num):
+# 올바른 데이터 값을 얻기 위해서 역순으로 정렬을 시킴
+
+# 정렬을 할때는 조건의 역순으로 거슬러 올라가며 정렬을 시키면
+# 원하는 값을 얻을 수 있고, 정렬의 기준으로 자료형의 타입을 정해주는것이 꼼수이다.
+# 우선 사전순으로 정렬을 시킴
+data.sort(key=lambda x: x[0])
+# 수학 점수가 감소하는 순서로 정렬
+data.sort(key=lambda x: int(x[3]), reverse=True)  # 역순으로 정렬
+# 영어 점수가 증가하는 순서로 정렬
+data.sort(key=lambda x: int(x[2]))
+# 국어 점수가 감소한는 순서로
+data.sort(key=lambda x: int(x[1]), reverse=True)
+
+for name in data:
+    print(name[0])
