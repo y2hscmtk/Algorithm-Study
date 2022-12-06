eaa2b1b7dc20e0cead84def3867b03db3f635653
Y2K <y2_12@naver.com>
2022-12-06 23:54:07 +0900

    백준 11652번

---
+++ 백준 문제풀이/10867.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/10867
+
+'''
+중복 뺴고 정렬하기
+'''
+
+'''
+아이디어 : set()자료형은 중복을 고려하지 않으므로 set에 넣었다가 빼면 중복된 값이 없어짐
+'''
+
+x = int(input())
+
+n = list(map(int, input().split()))
+
+n = list(set(n))  # 중복 값 제거하고
+n.sort()  # 정렬한 후에
+
+for data in n:
+    print(data, end=' ')
