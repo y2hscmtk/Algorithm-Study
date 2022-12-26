52066216249998ae6ace84ee036ae5ee6eac125f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2022-12-26 21:53:20 +0900

    백준 10828번 : 스택

--- 백준 문제풀이/10828.py
+++ 백준 문제풀이/10828.py
@@ -13,3 +13,46 @@ empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
 top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 
 '''
+import sys
+
+input = sys.stdin.readline
+
+# 스택의 역할을 할 리스트 생성
+stack = []
+
+# 명령의 횟수
+n = int(input())
+
+# len 명령을 최대한 줄이기 위해 스택의 크기를 측정할 변수 생성
+count = 0
+
+for i in range(n):
+    # 명령 입력받기
+    command = list(map(str, input().split()))
+    # push 명령이 존재한다면 숫자와 분할
+    if command[0] == "push":
+        stack.append(int(command[1]))
+        count += 1  # 스택 내용물 추가
+    elif command[0] == "top":
+        # 스택이 비어있다면 -1 출력
+        if count == 0:
+            print(-1)
+        else:
+            # 가장 위에 있는 정수 출력
+            print(stack[-1])
+    elif command[0] == "size":
+        # 스택의 크기를 출력한다.
+        print(count)
+    elif command[0] == "empty":
+        # 스택이 비어있으면 1을 출력하고 아니면 0을 출력한다.
+        if count == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "pop":
+        # 스택이 비어있다면
+        if count == 0:
+            print(-1)
+        else:  # 스택이 비어있지 않다면
+            print(stack.pop())
+            count -= 1
