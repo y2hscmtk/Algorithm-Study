7eb77764967a4a4d31c614636b68c04e8873261b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-01-01 11:57:40 +0900

    백준 1108번(플3) : 아이디어 정리 시간있을때 시도해볼것

--- 백준 문제풀이/10866.py
+++ 백준 문제풀이/10866.py
@@ -7,10 +7,81 @@
 
 push_front X: 정수 X를 덱의 앞에 넣는다.
 push_back X: 정수 X를 덱의 뒤에 넣는다.
+
 pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
 size: 덱에 들어있는 정수의 개수를 출력한다.
 empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+
 front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 '''
+# 덱을 이용하기 위해 덱 import
+from collections import deque
+
+# 시간초과 판정을 피하기 위해
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+
+# 덱 생성
+deq = deque()
+
+for _ in range(n):
+    # 명령 입력받기
+    command = list(map(str, input().split()))
+
+    if "push" in command[0]:
+        # 푸시 명령에 한하여 command[1]이 존재함
+        x = command[1]
+        # push back인지 push front인지에 따라 다른 명령 수행
+        if command[0] == "push_front":
+            # push_front 명령은 정수 x를 덱의 앞에 넣는다.
+            deq.appendleft(x)
+        elif command[0] == "push_back":
+            # push_back 명령은 정수 x를 덱의 뒤에 넣는다.
+            deq.append(x)
+    elif command[0] == "pop_front":
+        # 덱에 정수가 없는 경우에는 -1을 출력한다.
+        if len(deq) == 0:
+            print(-1)
+        else:
+            # 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다.
+            print(deq.popleft())
+    elif command[0] == "pop_back":
+        # 덱에 정수가 없는 경우에는 -1을 출력한다.
+        if len(deq) == 0:
+            print(-1)
+        else:
+            # 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다.
+            print(deq.pop())
+    # 덱에 들어있는 정수의 개수를 출력한다.
+    elif command[0] == "size":
+        print(len(deq))
+    # 덱이 비어있으면 1을, 아니면 0을 출력한다.
+    elif command[0] == "empty":
+        if len(deq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "front":
+        # 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        if len(deq) == 0:
+            print(-1)
+        else:
+            # 덱의 가장 앞에 있는 정수를 출력한다.
+            item = deq.popleft()
+            deq.appendleft(item)
+            print(item)
+    elif command[0] == "back":
+        # 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        if len(deq) == 0:
+            print(-1)
+        else:
+            # 덱의 가장 뒤에 있는 정수를 출력한다.
+            item = deq.pop()
+            deq.append(item)
+            print(item)
