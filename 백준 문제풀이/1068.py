b5d4ba94fd4d1e7aa9c52ece994db0aadf97a942
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-01-18 21:12:32 +0900

    백준 1068번 : 단말노드 구하기, 풀이참조

--- 백준 문제풀이/1068.py
+++ 백준 문제풀이/1068.py
@@ -20,3 +20,37 @@
 
 이제 리프 노드의 개수는 1개이다.
 '''
+
+# 정점의 개수
+n = int(input())
+
+# 부모에 대한 정보 입력받기
+parent = list(map(int, input().split()))
+
+remove = int(input())
+
+# dfs를 통하여 제거 대상와 그 자식들 전부 제거
+
+
+def DFS(remove):
+    # True는 제거되었다는 의미
+    parent[remove] = -2
+    # remove의 자식들도 모두 제거
+    for i in range(n):
+        # 제거된 대상을 부모로 하는 노드가 있다면
+        if parent[i] == remove:
+            DFS(i)  # 해당 노드도 제거처리
+
+
+# 제거 처리
+DFS(remove)
+
+# parent배열에서 중복값 제거 => 같은 부모
+
+count = 0
+for i in range(len(parent)):
+    # 제거되지 않은 노드이면서, 해당 노드를 부모로 삼지 않아야함
+    if parent[i] != -2 and i not in parent:
+        count += 1
+
+print(count)
