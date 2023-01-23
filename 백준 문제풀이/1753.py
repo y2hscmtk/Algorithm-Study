0bdd496f6420045d07265f1eb9abe4f970a064a7
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-01-23 18:43:05 +0900

    백준 1753번:최소힙을 이용한 다익스트라 알고리즘,최소힙 추가학습필요

--- 백준 문제풀이/1753.py
+++ 백준 문제풀이/1753.py
@@ -24,69 +24,44 @@ u와 v는 서로 다르며 w는 10 이하의 자연수이다.
 
 # 해당문제는 단방향 그래프임에 유의
 
+import sys
+import heapq # 시간초과판정을 피하기 위해 최소힙사용
 v, e = map(int, input().split())  # v정점의 개수,e간선의 개수
 k = int(input())  # 탐색을 시작할 정점
 
+INF = sys.maxsize  # 무한을 표현하기 위해
+
 graph = [[] for _ in range(v+1)]  # 그래프 간선 저장용
 
-found = [False for _ in range(v+1)]  # 최단경로를 찾았는지에 대한 여부를 저장
-
 # 최단경로를 저장할 배열, 탐색 시작정점에서 정점u까지의 최단경로를 distance[u]로 표현한다.
-distance = ['INF']*(v+1)
+distance = [INF]*(v+1)
 
 for _ in range(e):
     # i에서j로 가는 길의 가중치 w
     i, j, w = map(int, input().split())
-    graph[i].append([j, w])  # u에서 v로의 경로와 가중치를 저장
+    graph[i].append([w, j])  # u에서 v로의 경로와 가중치를 저장
 
 # 다익스트라 알고리즘
+distance[k] = 0  # 시작 정점의 거리 0으로 초기화
+heap = []  # 최소힙으로 사용
+heapq.heappush(heap, [0, k])  # 가중치0, 시작정점 k을 heap에 heappush함
 
-# distance배열 초기화
-for node, weight in graph[k]:
-    distance[node] = weight  # k에서 각 정점까지의 최단경로 초기화
-distance[k] = 0  # 탐색 시작 정점은 0으로 표시
-found[k] = True  # 시작정점 방문처리
+while heap:
+    # heap에서 가중치가 가장 작은 노드를 pop(heappop)
+    weight, node = heapq.heappop(heap)
 
+    # # 기존 경로가 더 짧다면 distance업데이트x
+    # if distance[node] < weight:
+    #     continue
 
-# 시작정점을 기준으로 distance가 가장 작은 정점 u를 찾아 리턴
-def choose():
-    global distance, found
-    min_weight = (v+1)*10  # 가중치 w는 최대 10, 모든 정점을 지나갈경우의 최대가중치 10+10+10...
-    min_index = -1  # distance가 가장 작은 정점의 인덱스 리턴
-    for i in range(1, v+1):
-        # 해당정점으로의 길이 없다면 다음정점에서 탐색
-        if distance[i] == 'INF':
-            continue
-        # found배열에 소속되어있지 않으면서 가중치가 최소인 정점 탐색
-        if distance[i] < min_weight and not found[i]:
-            min_weight = distance[i]  # 최소 가중치 갱신
-            min_index = i  # 인덱스 저장
-    return min_index
-
-
-# 다익스트라 알고리즘 시작
-for i in range(1, v+1):
-    u = choose()  # 현재 distance에 기록된 정점중 최소가중치의 정점
-    found[u] = True  # found 처리(최단경로 탐색성공)
-    # distance배열의 값 업데이트
-    # 정점 u를 통해서 가는 길이 기존의 경로보다 작은값일때 업데이트
-    for j in range(1, v+1):
-        if not found[j]:  # 아직 found되지 않은 정점에 대하여
-            # 기존의 최단경로를 기록할 배열 생성
-            # u에서 각 정점까지의 경로를 저장 => weight[1] => u에서 1번 노드까지의 가중치
-            weight = ['INF']*(v+1)
-            for n, w in graph[u]:
-                weight[n] = w  # 값 저장
-            if weight[j] == 'INF':
-                continue  # u에서 j로의 경로가 없다면 다음 노드부터탐색
-            # u를 거쳐가면 기존의 경로보다 길이 단축되는지 확인
-            if distance[j] == 'INF':
-                distance[j] = distance[u] + weight[j]  # 기존에 경로가 없었다면 업데이트
-                continue
-            # 기존에 경로가 있다면 길이 비교
-            if distance[u] + weight[j] < distance[j]:
-                distance[j] = distance[u] + weight[j]  # 최단경로 업데이트
+    # node를 통해서 갈때 더 짧다면 distance배열 업데이트
+    for w, n in graph[node]:
+        if w+weight < distance[n]:  # k에서 n으로의 거리를 더 단축시킬수 있다면
+            distance[n] = w + weight  # 값 업데이트
+            # 최소힙에 삽입
+            heapq.heappush(heap, [w+weight, n])
 
 # 결과 출력
 for i in range(1, v+1):
-    print(distance[i])
+    # 경로가 없다면 INF 출력
+    print("INF" if distance[i] == INF else distance[i])
