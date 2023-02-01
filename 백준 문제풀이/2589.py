c3188a930ebc3a028238b9ace0a2b0bbeeb9527c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-01 15:45:34 +0900

    백준2589번 풀이 : 보물섬, bfs,브루트포스

--- 백준 문제풀이/2589.py
+++ 백준 문제풀이/2589.py
@@ -18,3 +18,50 @@
 '''
 모든 육지에 대해서 bfs를 수행한뒤(브루트포스), 최댓값을 갱신한다.
 '''
+from collections import deque
+row, col = map(int, input().split())
+
+# 지도 입력받기
+graph = [list(input()) for _ in range(row)]
+
+# 방향벡터
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+
+# bfs정의
+def bfs(start, end):
+    time = 0  # 최단 시간을 기록하기 위함
+    queue = deque()
+    queue.append([start, end])
+    # 방문정보를 기록할 배열 생성
+    visited = [[-1]*col for _ in range(row)]
+    visited[start][end] = 0  # 방문처리
+    while queue:
+        x, y = queue.popleft()
+        # 상하좌우 검사
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 지도의 범위를 벗어나지 않으면서 땅이라면
+            if 0 <= nx < row and 0 <= ny < col and graph[nx][ny] == 'L':
+                # 아직 방문하지 않았다면 갱신
+                if visited[nx][ny] == -1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    # 더 큰값으로 time값 갱신 => 최장거리에 보물이 묻혀있으므로
+                    time = max(time, visited[nx][ny])
+                    queue.append([nx, ny])  # 큐에 좌표 삽입
+    return time
+
+
+result = 0  # 보물이 묻혀 있는 두 곳 사이를 최단 거리로 이동하는 시간
+
+# 브루트포스
+# 땅을 발견하면 해당위치에서 bfs를 수행하여 최장거리 업데이트
+for i in range(row):
+    for j in range(col):
+        # 땅을 발견하면 bfs => 최장거리 리턴
+        if graph[i][j] == 'L':
+            result = max(result, bfs(i, j))
+
+print(result)
