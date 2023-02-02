c190b80408cc1a3989344b96f0b707c347e58f34
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-02 23:52:40 +0900

    백준 3055번: 3번째 인자를 이용하여 홍수와 고슴도치를 구별 => nameerror

--- 백준 문제풀이/3055.py
+++ 백준 문제풀이/3055.py
@@ -26,18 +26,23 @@ for i in range(r):
         elif graph[i][j] == '*':
             # 홍수 초기위치 지정
             flood_x, flood_y = i, j
+        elif graph[i][j] == 'D':
+            # 목적지 지정
+            result_x, result_y = i, j
 
 # 큐 생성 => 홍수가 먼저 bfs를 진행하고, 이후 고슴도치의 bfs가 진행되야함
 # 순서에 유의하여 큐에 삽입
 queue = deque()
 # 홍수가 먼저 탐색을 진행할수 있도록 먼저 삽입
-queue.append([flood_x, flood_y])
-queue.append([hedgehog_x, hedgehog_y])
+# 세번째 인자로 True와 False를 줘서 True이면 홍수의 움직임을 의미하도록 하고,
+# False이면 고슴도치의 움직임을 의미하도록
+queue.append([flood_x, flood_y, True])
+queue.append([hedgehog_x, hedgehog_y, False])
 
 # 홍수에 대한 방문정보를 저장하기 위한 배열
 flooded = [[False]*c for _ in range(r)]
 flooded[flood_x][flood_y] = True  # 홍수의 시작위치는 침수되어 있어야함
-# 고슴도치에 대한 방문정보를 저장하기 위한 배열 => 시간을 누적시켜 저장함
+# # 고슴도치에 대한 방문정보를 저장하기 위한 배열 => 시간을 누적시켜 저장함
 visited = [[-1]*c for _ in range(r)]
 visited[hedgehog_x][hedgehog_y] = 0  # 시작위치는 방문처리 시작위치에 도달하는데 걸린 시간 0초
 
@@ -50,34 +55,32 @@ dy = [-1, 1, 0, 0]
 # 홍수와, 고슴도치가 번갈아가며 이동하도록 bfs를 작성할것
 def bfs(queue):
     while queue:
-        x, y = queue.popleft()
+        x, y, isFlood = queue.popleft()
         # 움직이는 대상이 고슴도치인지 홍수인지 구별
-        # 움직이는 대상이 홍수일경우
-        if graph[x][y] == '*' or flooded[x][y]:
-            # 네 방향에 대해 탐색 진행
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                # 배열의 범위를 벗어나지않으면서 침수되지않은 지역의 경우만 방문
-                if 0 <= nx < r and 0 <= ny < c and not flooded[nx][ny]:
-                    # 홍수는 비버의 집과 돌을 통과하지 못함
-                    if graph[nx][ny] != 'D' and graph[nx][ny] != 'X':
-                        flooded[nx][ny] = True  # 침수처리
-                        queue.append([nx, ny])  # 큐에 좌표삽입
-        else:  # 움직이는 대상이 고슴도치일경우
-            # 고슴도치가 비버의 집에 도달하였는지 확인
-            if graph[x][y] == 'D':
-                return visited[x][y]  # 방문하는데 걸린 시간 리턴
-            # 네 방향에 대해 탐색 진행
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                # 배열의 범위를 벗어나지않으면서 방문하지 않은 지역으로만 이동
-                if 0 <= nx < r and 0 <= ny < c and visited[nx][ny] == -1:
-                    if graph[nx][ny] != '*':  # 물이 아니라면 방문가능
-                        visited[nx][ny] = visited[x][y] + 1  # 시간누적하여 방문처리
-                        queue.append([nx, ny])
+        # 네 방향에 대해 탐색 진행
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+            # 배열의 범위를 벗어나지 않는지와, 인접좌표가 돌이 아닌경우에 대해서 이동
+            if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] != 'X':
+                # 움직이는 대상이 홍수일경우
+                if isFlood:
+                    # 이전에 방문하지 않은 정점에 대하여 flooded배열 수정
+                    # 홍수는 비버의 집과 돌은 통과하지 못함
+                    if not flooded[nx][ny] and graph[nx][ny] != 'D':
+                        flooded[nx][ny] = True  # 침수처리
+                        queue.append([nx, ny, True])  # 홍수의 좌표와, 홍수여부 큐에 삽입
+                # 움직이는 대상이 고슴도치일경우
+                else:
+                    # 고슴도치의 위치가 목적지에 도달하였다면 정답 리턴
+                    if x == result_x and y == result_y:
+                        return visited[x][y]  # 방문하는데 걸린 시간 리턴
+                    # 고슴도치는 침수될지역(침수된지역)으로 이동하지 못하고, 돌을 통과하지못함
+                    # 또한 방문하지 않은 지역에 한해서 방문해야함
+                    if not flooded[nx][ny] and visited[nx][ny] == -1:  # 아직 방문하지 않은 지역에 한해서
+                        visited[nx][ny] = visited[x][y] + 1
+                        queue.append([nx, ny, False])  # 움직이는 대상이 두더지임을 큐에 삽입
     # 비버가 도착하지 못한채 bfs가 종료되면 KAKTUS 리턴
     return "KAKTUS"
 
