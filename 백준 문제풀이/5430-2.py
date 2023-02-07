bdf79c309161453f0b7df4b54f4331ad18d92acb
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-07 15:16:27 +0900

    백준 5430번 : 시간초과 판정 => 덱으로 전략 수정

--- 백준 문제풀이/5430-2.py
+++ 백준 문제풀이/5430-2.py
@@ -16,3 +16,49 @@ AC는 정수 배열에 연산을 하기 위해 만든 언어이다.
 다음 줄에는 [x1,...,xn]과 같은 형태로 배열에 들어있는 정수가 주어진다. (1 ≤ xi ≤ 100)
 전체 테스트 케이스에 주어지는 p의 길이의 합과 n의 합은 70만을 넘지 않는다.
 '''
+from collections import deque
+# 테스트 케이스의 수
+t = int(input())
+
+for i in range(t):
+    # 수행할 명령
+    commands = list(input())
+    # 입력받을 데이터의 수
+    n = int(input())
+    # 입력받을 데이터
+    data = input()
+    # 데이터 분할
+    data = data.replace('[', ',').replace(']', ',')
+    # ,를 기준으로 단어 나누기
+    data = data.split(',')
+    # 에러 없이 명령이 수행됐는지 확인하기 위해서
+    error = False
+    # 알고리즘 수행
+    for command in commands:
+        # 뒤집기 명령
+        if command == 'R':
+            data.reverse()
+        # 첫번째 단어 제거 명령
+        elif command == 'D':
+            # 제거할 단어가 없다면 error출력하고 다음 명령 입력받기
+            if data[1] == '':
+                print('error')
+            # if len(data) == 2:
+            #     print('error')
+                error = True  # 에러발생 처리
+                break
+            else:
+                # 제거할 단어가 남아있다면 제거
+                del data[1]
+
+    # 에러가 있었다면 결과출력없이 다음 테스트케이스로 넘어가기
+    if error:
+        continue
+    # 단어 출력하기
+    print('[', end='')
+    for j in range(1, len(data)-1):
+        if j == len(data)-2:
+            print(data[j], end='')
+        else:
+            print(data[j], end=',')
+    print(']')
