9bb0f78c8e8e6cceafcbdd7a54dc9c1d552d6ec1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-12 16:51:59 +0900

    백준 16234번 인구이동, 여러개의 그룹일 경우를 고려

--- 백준 문제풀이/16234.py
+++ 백준 문제풀이/16234.py
@@ -21,3 +21,68 @@ r행 c열에 있는 나라에는 A[r][c]명이 살고 있다. 인접한 나라 �
 조건을 만족한다면 해당 정점을 대상으로 bfs를 수행하여 조건을 만족하는 정점들을 연합으로 삼고, bfs가 종료된후 인구이동을 실시한다.
 bfs를 수행하는 횟수를 카운트하여 최종 정답으로 출력한다.
 '''
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# nxn 크기의 배열, 인구차이 조건 l명이상 r명 이하
+n, l, r = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+# 인접한 방향에 대한 정의
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+day = 0  # 인구이동에 걸리는 시간을 기록하기 위한 변수
+
+
+def bfs(s, e):
+    queue = deque()
+    queue.append([s, e])
+
+    visited[s][e] = True  # 탐색 시작정점은 방문처리
+    # 연합에 포함되는 좌표들을 기록할 배열
+    group = [[s, e]]
+    total_people = graph[s][e]  # 연합에 포함되는 총 인구수를 계산하기 위해
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 영역을 벗어나지 않으면서 처음방문하는 좌표를 대상으로
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                # 조건을 만족한다면 => 연합에 포함된다면, 삽입
+                if l <= abs(graph[nx][ny]-graph[x][y]) <= r:
+                    visited[nx][ny] = True  # 방문처리
+                    group.append([nx, ny])  # 연합에 해당 국가 등록
+                    queue.append([nx, ny])  # 좌표 큐에 삽입
+
+    return group  # 연합정보를 리턴
+
+
+# 모든 좌표의 정점에 대해 bfs를 실시할지 여부를 계산
+for m in range(n*n):
+    move = False
+    # 방문정보를 기록할 배열
+    visited = [[False]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == False:
+                visited[i][j] = True  # 방문처리
+                group = bfs(i, j)
+                # 그룹이 한개 이상인 경우에만 인구이동
+                if len(group) != 1:
+                    # 연합의 총 인구수 계산
+                    total_population = 0
+                    for x, y in group:
+                        total_population += graph[x][y]
+                    # 새로운 인구 지정
+                    new_population = total_population//len(group)
+                    # 인구 이동
+                    for x, y in group:
+                        graph[x][y] = new_population
+                    move = True
+    if move:
+        day += 1  # 하루 더하기
+print(day)
