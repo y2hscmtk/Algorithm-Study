ae35f1fd7064df90485580d75f6f56946f7b17a9
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-13 21:13:25 +0900

    1005번 => 위상정렬 + dp 활용한 문제, 위상정렬 공부후 다시 풀어보기

--- 백준 문제풀이/1005.py
+++ 백준 문제풀이/1005.py
@@ -12,3 +12,23 @@
 
 마지막 줄에는 백준이가 승리하기 위해 건설해야 할 건물의 번호 W가 주어진다.
 '''
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    # 건물의 개수 n개, 건물간 건설 규칙의 순서
+    n, k = map(int, input().split())
+    graph = [[] for _ in range(n+1)]  # 건설 규칙의 순서를 기록하기 위함
+    build = [False for _ in range(n+1)]  # 건물을 다 지었는지를 체크하기 위함
+    # 각 건물을 짓는데 걸리는 시각(cost) 기록
+    cost = list(map(int, input().split()))
+    # 각 건물을 짓기 위한 요구사항을 기록하기 위한 배열
+    require = [[] for _ in range(n+1)]
+    for j in range(k):
+        x, y = map(int, input().split())
+        graph[x].append(y)  # 건물 x를 지은후 y를 지을수 있음을 의미
+        require[y].append(x)  # 건물 y를 짓기 위해서 x를 먼저 지어야됨을 의미
+    w = int(input())  # 승리를 위해 지어야하는 건물 w
