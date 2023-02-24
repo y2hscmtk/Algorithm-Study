0945e1d44146b38a3b0eef49e220c97f34ac1f73
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-24 19:23:33 +0900

    백준 14889번 : 스타트와 링크, 브루트포스, 조합

--- 백준 문제풀이/14889.py
+++ 백준 문제풀이/14889.py
@@ -21,3 +21,41 @@ i\j	1	2	3	4
 
 두 팀의 능력치 차이의 최솟값을 구하라
 '''
+import sys
+from itertools import combinations
+
+n = int(input())  # 인원수
+
+member = [i for i in range(1, n+1)]  # 멤버 표시
+member = set(member)
+
+synergy = [list(map(int, input().split())) for _ in range(n)]  # 시너지 입력받기
+
+# Sij는 i행 j열과 j행 i열의 합과 같다.
+
+# 계산할수 있는 모든 경우의 수를 조합으로 구하고, 2중 반복문으로 나눠진 배열에서 각각 시너지 합을 구한뒤,
+# 두 차가 최소가 되도록 업데이트한다.
+
+# 두 팀의 능력치 차를 저장할 변수(정답)
+result = sys.maxsize
+
+# 팀을 절반으로 나누기 위해 절반을 뽑고, 기존 배열에서 해당 수들을 제외한 값들로 새로운 배열을 만든다.
+for array in combinations(member, n//2):
+    # 두 팀 만들기
+    team_start = array
+    team_link = member-set(array)
+
+    # 만들어진 두 팀에 대해서 각각의 시너지 구하기
+    team_start_stats = 0
+    team_link_stats = 0
+
+    # 스타트팀의 능력치 합 구하기
+    for i, j in combinations(team_start, 2):
+        team_start_stats += (synergy[i-1][j-1] + synergy[j-1][i-1])
+    # 링크팀의 능력치 합 구하기
+    for i, j in combinations(team_link, 2):
+        team_link_stats += (synergy[i-1][j-1] + synergy[j-1][i-1])
+
+    # 두 팀의 능력치 차 갱신
+    result = min(result, abs(team_start_stats-team_link_stats))
+print(result)
