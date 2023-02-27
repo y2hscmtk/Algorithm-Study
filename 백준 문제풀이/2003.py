b34a3bec31d2ef8f0fa0a7fb7a29d02a109f9978
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-27 19:56:28 +0900

    백준 2003번 풀이 : 수들의 합2, 경우의 수, 브루트포스

--- 백준 문제풀이/2003.py
+++ 백준 문제풀이/2003.py
@@ -15,3 +15,18 @@ N개의 수로 된 수열 A[1], A[2], …, A[N] 이 있다.
 아이디어 : 배열의 앞에서부터 반복하여 순서대로 수를 더하고, M이 되었다면 반복을 멈추고 다음수부터 이어서 그 과정을 반복한다.
 이 과정에서 모든 경우의 수를 구한다.
 '''
+n, m = map(int, input().split())
+
+number = list(map(int, input().split()))
+
+result = 0  # 경우의 수의 개수를 기록할 변수
+
+for i in range(n):
+    number_sum = 0  # 숫자의 합을 누적시키기 위해
+    for j in range(i, n):
+        number_sum += number[j]  # 수를 누적시켜 더한후,
+        if number_sum == m:  # 만들고자 하는 수가 되었는지 확인
+            result += 1  # 만들고자 하는 수가 완성되었다면 경우의 수를 추가하고
+            break  # 해당 수에 대한 반복을 종료한다.
+
+print(result)
