124f716ebf868c8ad8895b98e1af0dc6cedfc542
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-02-28 17:13:16 +0900

    백준 9935번 : 2번째 아이디어

--- 백준 문제풀이/9935.py
+++ 백준 문제풀이/9935.py
@@ -16,35 +16,13 @@
 반복문을 이용하여 문자열의 모든 문자를 하나씩 검사하여, 찾고자하는 문자열의 첫번째 문자가 일치하면
 해당 문자를 기준으로 다시 찾고자 하는 문자열과 일치하는지 검사하여 del 명령으로 일치하는 문자를 지운후 다음 문자부터 반복한다.
 반복이 종료된 이후, 문자열의 길이를 확인하여 남아있는 문자가 없다면 FRULA를 출력하고. 남아있는 문자가 있다면 해당 문자를 출력한다
+=> 시간초과
 '''
-# 원본 문자열
-words = list(input())
-# 폭발시킬 문자열
-search = list(input())
-
-i = 0
-while i != len(words):
-    index = 0
-    flag = True
-    if words[i] == search[index]:
-        index += 1
-        s = i  # 시작 인덱스
-        correct = True
-        for j in range(i+1, i+len(search)):
-            if words[j] == search[index]:
-                index += 1
-            else:
-                correct = False
-                break
-        if correct:  # 문자열이 일치한다면
-            del words[s:j+1]
-            i = 0  # 다시 첫번째 인덱스로 변환
-            flag = False  # i값 중복변환 방지
-    if flag:
-        i += 1  # 다음 인덱스로
-
-if len(words) != 0:
-    for word in words:
-        print(word, end='')
-else:
-    print("FRULA")
+'''
+아이디어2:
+기존의 아이디어는 반복을 여러번하기때문에 시간초과 판정을 받기쉬움 따라서 스택을 이용하여 문제를 해결
+1. 스택에 문자를 푸쉬한다.
+2. 스택의 top과 탐색하고자 하는 문자열의 맨끝 문자가 일치하는지 확인
+3. 일치한다면 탐색하고자하는 문자열의 길이만큼 차례로 팝해서 문자가 모두 일치하는지 확인
+위 과정을 더 이상 일치하지 않을때까지 반복하면됨
+'''
