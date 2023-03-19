fca7172373ac3ac6b24ffb9751dabb4b57d394e1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-03-19 17:04:54 +0900

    백준 17829번 풀이 : 222-풀링, 구현

--- 백준 문제풀이/17829.py
+++ 백준 문제풀이/17829.py
@@ -13,3 +13,35 @@
 
 마지막에 남은 수를 출력한다.
 '''
+n = int(input())
+
+# 배열 입력받기
+array = [list(map(int, input().split())) for _ in range(n)]
+
+
+# 222풀링 함수 정의
+# 배열을 매개변수로 받아서, 새로운 배열을 만들어 리턴
+def tttpooling(array):
+    global n  # 배열의 사이즈 n*n
+    # 새로운 배열 생성
+    new_array = [[0]*(n//2) for _ in range(n//2)]
+
+    # 풀링 작업 수행
+    for i in range(0, n, +2):
+        for j in range(0, n, +2):
+            # 네가지 수들중 두번째로 큰 수 찾기
+            sorted_data = [array[i][j], array[i+1][j],
+                           array[i][j+1], array[i+1][j+1]]
+            sorted_data.sort(reverse=True)  # 내림차순으로
+            new_array[i//2][j//2] = sorted_data[1]
+
+    n //= 2  # n의 크기 줄이기
+    return new_array
+
+
+# 숫자가 한개 남을때까지 풀링함수를 반복한다.
+while True:
+    if len(array) == 1:
+        print(*array[0])
+        break
+    array = tttpooling(array)
