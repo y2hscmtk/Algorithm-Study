2ca5247b3dd7846eaef3cb1f19803b4f53fcdcb1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-03-28 17:52:12 +0900

    백준 9237번 풀이 : 그리디, 정렬

--- 백준 문제풀이/9237.py
+++ 백준 문제풀이/9237.py
@@ -6,3 +6,22 @@
 
 둘째 줄에는 각 나무가 다 자라는데 며칠이 걸리는지를 나타낸 ti가 주어진다. (1 ≤ ti ≤ 1,000,000)
 '''
+n = int(input())  # 묘목의 수
+
+# 전체 묘목
+tree_list = list(map(int, input().split()))
+
+# 자라는데 오래걸리는 나무순으로 나무를 심어야함
+# 내림 차순 정렬
+tree_list.sort(reverse=True)
+
+# 나무를 다 심은 이후시점에서 가장 오래걸리는 나무의 일수를 day에 더해주면됨
+# 나무를 다 심으려면 => 일수에서 하루씩 줄여나가며 빼주면됨
+for i in range(n):
+    tree_list[i] -= (n-i-1)
+
+day = n + 1  # 나무를 다 심는데 걸린 시간이 n+1(n개의 나무, 구매일 1일)
+
+# 남은 나무들 중 가장 오래걸리는 일수를 더해줌
+day += max(tree_list)
+print(day)
