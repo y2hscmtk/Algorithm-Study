0484e9123998c99dd3268f5355569d3967dfbb1d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-03-30 21:39:12 +0900

    백준 16926번 => 나중에 풀어보기

--- 백준 문제풀이/16926.py
+++ 백준 문제풀이/16926.py
@@ -4,3 +4,11 @@
 배열은 반시계방향으로 돌린다.
 nxm 배열을 r번 회전시킨 결과를 출력하라.
 '''
+n, m, r = map(int, input().split())
+array = list(map(int, input().split()) for _ in range(n))
+
+
+def turn():
+    for _ in range(n//2):
+        temp = array[0][0]
+        # for i in range(m):
