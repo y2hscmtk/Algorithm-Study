1e9f45200868e9af3edc5291217f5f860cc2c6b5
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-04-26 18:01:04 +0900

    백준 1213번 펠린드롬 풀이 : 브루트포스, 문자열, 규칙찾기

--- 백준 문제풀이/1213.py
+++ 백준 문제풀이/1213.py
@@ -1,73 +1,43 @@
 # https://www.acmicpc.net/problem/1213
-
-'''
-임한수와 임문빈은 서로 사랑하는 사이이다.
-
-임한수는 세상에서 팰린드롬인 문자열을 너무 좋아하기 때문에, 둘의 백일을 기념해서 임문빈은 팰린드롬을 선물해주려고 한다.
-
-임문빈은 임한수의 영어 이름으로 팰린드롬을 만들려고 하는데, 임한수의 영어 이름의 알파벳 순서를 적절히 바꿔서 팰린드롬을 만들려고 한다.
-
-임문빈을 도와 임한수의 영어 이름을 팰린드롬으로 바꾸는 프로그램을 작성하시오.
-'''
-
-'''
-아이디어
-총 글자의 수가 짝수개 일때와 홀수개 일때의 상황
-1. 글자수가 짝수개일때
-홀수 글자의 수는 홀수 개 있어야함 짝수 글자의 수는 중요하지않음 AABBCC, AABB, AAABB,
-1개만 있는 글자는 한개도 존재해서는 안됨
-2. 글자수가 홀수개일때
-홀수 글자의 수는 홀수 개 있어야함 AAABB AAA
-1개만 있는 글자는 1개만 존재할수 있음 => 이 경우에 다른 모든 글자는 짝수개 이어야함
-
-=> 결론적으로 짝수개의 글자는 짝이 다 맞으므로, 짝수 몇쌍이 존재하던 상관없음
-홀수개의 쌍은 짝수개일때는 존재해선 안되며, 홀수개일때는 홀수개 있어야함, 1개만 있는 글자는 한개만 존재할수 있음
-
-=> 9퍼센트에서 실패
-'''
-
-
-'''
-새로운 아이디어
-
-아몰라 그냥 배치하고 펠린드롬검사해서 실패하면 아임쏘리 출력할래
-'''
 import sys
-w = input()  # 문장을 입력받음
+# 초기 문자 입력받기
+data = list(input())
 
-alphabet = [w[0]]  # 글자들을 저장할 배열
-alphabet_count = [1]
-for i in range(1, len(w)):
-    flag = True
-    for j in range(0, len(alphabet)):  # 글자 배열에 저장된 단어들을과 비교
-        if alphabet[j] == w[i]:  # i번째 문자와 글자배열의 해당 글자가 같다면
-            alphabet_count[j] += 1
-            flag = False
-    if flag:
-        alphabet.append(w[i])
-        alphabet_count.append(1)
+# 정답이 여러개 일때는 사전순으로 앞에 나가는것을 출력한다.
+data.sort()
 
-# 아몰라 그냥 일단 배치해
-pl = ['A']*len(w)
-k = 0
-check = True
-for i in range(len(alphabet)):
-    if (check):
-        if alphabet_count[i] % 2 != 0:
-            pl[len(w)//2] = alphabet[i]
-            check = False
-    else:
-        print("I'm Sorry Hansoo")  # 거 미안하게 됐수다
-        sys.exit(0)
-    for j in range(k, alphabet_count[i]//2+k):
-        pl[j] = alphabet[i]
-        pl[len(w)-1-j] = alphabet[i]
-    k += (alphabet_count[i]//2)
+# 먼저 전체 알파벳 개수를 파악한다. dict이용
+dict = {}
+for a in data:
+    # 처음 기록되는 알파벳이라면 1로 초기화
+    if a not in dict:
+        dict[a] = 1  # 개수 세기
+    else:  # 기존에 있는 알파벳이라면 +1
+        dict[a] += 1
 
-# 팰린 뭐시기 그거 검사 실시
-for i in range(len(pl)//2):
-    if pl[i] != pl[len(pl)-1-i]:  # 두 단어가 일치하지 않는 경우
-        print("I'm Sorry Hansoo")  # 거 미안하게 됐수다
-        sys.exit(0)
+p = []
+error_count = 0  # 홀수개인 단어가 몇개인지 확인하는 용
+last = []
+# 딕셔너리에서 키 하나씩 뽑으면서 배치하기(왼쪽에 절반만 배치)
+for alpha in dict:
+    # 팰린드롬에 단어 배치
+    # 짝수개인지 홀수개인지 확인
+    if dict[alpha] % 2 != 0:
+        last.append(alpha)  # 마지막에 올 단어 하나 삽입
+        error_count += 1  # 홀수개일경우 에러카운트 증가
+    # 짝수든 홀수든 절반만큼 배치
+    n = dict[alpha]//2
+    p.append(alpha*n)  # 절반만큼 배치
+# 서로 다른 문자일때만 에러카운트가 증가하므로
+    # 에러카운트가 2개 이상이라면 => 펠린드롬을 완성할수 없음
+if error_count >= 2:
+    print("I'm Sorry Hansoo")  # 미안하게 됐수다
+    sys.exit(0)  # 프로그램 종료
 
-print(''.join(pl))  # 완성된 단어
+
+for a in p:
+    print(a, end='')
+if len(last) != 0:
+    print(last[0], end='')
+for a in range(len(p)-1, -1, -1):
+    print(p[a], end='')
