67405620b5126ccfa14ecf92ccc1349da7b69151
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-05-12 14:59:47 +0900

    백준 10025번 게으른 백곰 풀이 : 슬라이스 윈도우

--- 백준 문제풀이/10025.py
+++ 백준 문제풀이/10025.py
@@ -13,36 +13,27 @@
 n,k = map(int,input().split())
 
 max_index = -1
-result = 0 # 가져갈수 있는 얼음의 최대값 
-data = []
+
+ice = [0]*1000001 # 얼음의 누적합 기록용 배열
 
 for i in range(n):
     g, x = list(map(int,input().split()))
+    ice[x] = g # x위치에 g만큼 얼음이 있다.
     max_index = max(max_index,x) # 최대 좌표를 알기 위해
-    data.append([x,g]) # x좌표에 g만큼의 얼음이 있다.
-data.sort() # 좌표를 기준으로 오름차순 정렬
-
-ice = [0]*(max_index+k+1) # 얼음의 누적합 기록용 배열
-
-for x,g in data:
-    # x보다 높은 인덱스에 g저장
-    for i in range(x,len(ice)):
-        ice[i] += g # 얼음 누적 기록
 
 
-# 가져갈수 있는 얼음의 양이 갱신되는지 확인 => 갱신된다면 True리턴
-def check(m):
-    temp = ice[m+k] - ice[m-k] # 가져갈수 있는 얼음의 양 기록
-    if temp>result: #더 많이 가져갈수 있다면
-        result = temp # result값 갱신
-        return True
-    else:
-        return False
+# 윈도우 크기 설정
+size = 2*k + 1 # 좌우로 k만큼, 그리고 해당인덱스+1
 
+# 윈도우 범위 내의 초기 합
+window = sum(ice[:size]) 
 
-# 가져갈수있는 얼음의 양이 최대값이 될때까지 이분탐색
-# 적정 좌표 탐색
-low,high = 1, max_index # max_index로부터 오른쪽으로 k만큼 떨어진 곳까지 탐색이 가능하므로
-while low<high:
-    mid =(low+high)//2
+result = window # 최대로 가져갈수있는 얼음의 양
+
+for i in range(size,max_index+1):
+    # 다음 인덱스의 값을 더하고, 기존 값의 가장 첫 인덱스를 뺀다.
+    # size가 유지된다~
+    window += (ice[i]-ice[i-size]) # 창문을 한칸씩 민다고 생각 => 새로운 창을 추가하고, 가장 앞의 창을 제거
+    result = max(result,window)
     
+print(result)
