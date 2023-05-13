e103f18e53fc76470853b46215e8d4fcbf06b20f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-05-13 19:19:12 +0900

    백준 2776번 암기왕 풀이 : 이분탐색

--- 백준 문제풀이/2776.py
+++ 백준 문제풀이/2776.py
@@ -8,3 +8,35 @@
 모든 정수들의 범위는 int 로 한다.
 각각의 수에 대하여, ‘수첩1’에 있으면 1을, 없으면 0을 출력하는 프로그램을 작성해보자.
 '''
+
+
+# 이분탐색 정의
+def check(num):
+    low,high = 0,n # 인덱스 0부터 인덱스 n까지..
+    while low<high:
+        mid = (low+high)//2 # 중간 인덱스
+        # mid번째 인덱스의 값과 num비교
+        # 일치한다면
+        if data1[mid] == num:
+            return 1
+        elif data1[mid] < num: # 더 작다면
+            low = mid+1 # 오른쪽에서 탐색하기
+        elif data1[mid] > num: # 더 크다면
+            high = mid # 왼쪽에서 탐색하기
+    # 탐색 결과 원하는 숫자를 찾지 못했다면
+    return 0
+
+
+for _ in range(int(input())): # t번만큼 반복
+    n = int(input())
+    # 수첩 1에 n개만큼 데이터 입력받기
+    data1 = list(map(int,input().split()))
+    m = int(input())
+    # 수첩 2에 m개만큼 데이터 입력받기
+    data2 = list(map(int,input().split()))
+
+    data1.sort() # 이분탐색을 위해 오름차순 정렬
+    # 수첩2의 수들이 수첩1에 있으면 1을, 없으면 0을 출력한다.
+    for num in data2:
+        print(check(num)) # num이 data1에 있으면 1, 없으면 0
+        
