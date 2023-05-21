44993c74429ac4176cdf1835912a36bf54f19410
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-05-21 15:12:27 +0900

    백준 2358번 평행선 풀이 : 딕셔너리

--- 백준 문제풀이/2358.py
+++ 백준 문제풀이/2358.py
@@ -8,3 +8,29 @@ x[] 와 y[] 를 만들어서 좌표별로 몇개씩 있는지 확인한다.
 예를 들어 [[0,0],[10,10],[0,10],[10,0]] 의 경우
 x[0] = 2, x[10] = 2, y[0] = 2, y[10] = 2 가 되며, 모두 2 이상이므로 4개의 직선이 그어지게 된다.
 '''
+# 각 좌표에 해당하는 점이 몇개인지 카운팅 하기 위해
+dict_x = {}
+dict_y = {}
+# px = [0]*100001
+# py = [0]*100001
+
+for _ in range(int(input())): # n만큼 데이터 입력받기
+    x,y = map(int,input().split())
+    # 각 좌표 카운트 증가
+    if x not in dict_x:
+        dict_x[x] = 1
+    else:
+        dict_x[x] += 1
+    if y not in dict_y:
+        dict_y[y] = 1
+    else:
+        dict_y[y] += 1
+
+count = 0
+for key in dict_x:
+    if dict_x[key] >=2:
+        count+=1
+for key in dict_y:
+    if dict_y[key] >=2:
+        count+=1
+print(count)
