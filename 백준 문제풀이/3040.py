bfcc9fe197703f5caa4d0a61f2551454a959b163
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-05-30 18:15:06 +0900

    백준 3040번 백설공주와 일곱 난쟁이 풀이 : 브루트포스,조합

---
+++ 백준 문제풀이/3040.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/3040
+from itertools import combinations
+
+# 브루트포스
+# 더해서 100이 되는 수를 출력 => 조합 이용
+n = [int(input()) for _ in range(9)] # 각 난쟁이 키 저장
+
+# 난쟁이들 중에서 7명 뽑아서 키 더해보고, 100되면 출력
+for array in combinations(n,7):
+    if sum(array) == 100:
+        print(*array, sep='\n') # 100이 된다면 키 하나씩 출력
