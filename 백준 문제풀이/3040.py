f20d1fa6f01eebd6a9d15b3aa9e43ad6bf368d0d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-06-04 12:46:37 +0900

    백준 9086번 문자열 풀이 : 문자열

--- 백준 문제풀이/3040.py
+++ 백준 문제풀이/3040.py
@@ -3,7 +3,9 @@ from itertools import combinations
 
 # 브루트포스
 # 더해서 100이 되는 수를 출력 => 조합 이용
-n = [int(input()) for _ in range(9)] # 각 난쟁이 키 저장
+n = []
+for _ in range(9):
+    n.append(int(input())) # 각 난쟁이 키 입력받아 저장
 
 # 난쟁이들 중에서 7명 뽑아서 키 더해보고, 100되면 출력
 for array in combinations(n,7):
---
+++ 백준 문제풀이/9086.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/9086
+for _ in range(int(input())):
+    data = input()
+    print(data[0]+data[-1])
