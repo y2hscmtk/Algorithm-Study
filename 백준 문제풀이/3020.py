c67d6ce0fb0422c7c6047a72f5504201aee3f507
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-06-14 22:31:23 +0900

    백준 3020번 개똥벌레 풀이 : 이분탐색, bisect 라이브러리

--- 백준 문제풀이/3020.py
+++ 백준 문제풀이/3020.py
@@ -16,3 +16,47 @@ low=1,high=3, middle=2, 2에서 조건을 만족 => 인덱스 2번 이후의 값
 
 석순과 종순에 대해서 카운트 수행하고, 최소값을 업데이트한다. 각 카운팅 횟수에 대해 사전에 배열에 기록해두고 최소값과 같은 값의 수를 출력한다.
 '''
+import sys
+from bisect import bisect_left,bisect_right # 이분탐색을 도와주는 라이브러리
+N,H = map(int,input().split()) # N개의 장애물이 존재(짝수인덱스는 석순, 홀수 인덱스는 종유석), H는 높이
+
+stalagmite = [] # 석순
+stalactite = [] # 종유석
+
+for i in range(N):
+    size = int(input())
+    if i%2==0: # 석순이면 석순 배열에
+        stalagmite.append(size)
+    else: # 종유석이면 종유석 배열에 H-value로 삽입
+        stalactite.append(H-size)
+        
+# 입력이 끝나면 각 배열 정렬
+stalagmite.sort() # 오름차순 정렬
+stalactite.sort() # 오름차순 정렬
+
+# 각 배열의 크기
+stalagmite_size = len(stalagmite)
+stalactite_size = len(stalactite)
+
+min_count = sys.maxsize # 최소 횟수
+# 층별로 부딛히는 횟수를 저장하기 위한 배열
+crash_count = []
+
+# 높이 1부터 높이 H까지 각 배열에 대해서 이분탐색 수행
+for i in range(1,H+1):
+    count = 0 # 장애물과 부딛히는 횟수 초기화
+    
+    # 석순과 부딛히는 횟수(i와 크거나 같을때)
+    index = bisect_left(stalagmite,i) # 해당 값과 동일한 값들 중, 가장 왼쪽 인덱스
+    count += stalagmite_size-index # 전체 석순 개수에서 빼주면 몇개 충돌하는지 알 수 있음
+    
+    # 종유석 충돌 계산
+    index = bisect_right(stalactite,i-1) # 오른쪽 값 반환
+    count += index # index가 충돌 횟수가 됨
+    
+    crash_count.append(count) # 부딛힌 횟수 기록
+    min_count = min(min_count,count) # 최소 횟수 업데이트
+    
+print(min_count,end=' ')
+# 최소횟수인 값의 수 출력
+print(crash_count.count(min_count))
