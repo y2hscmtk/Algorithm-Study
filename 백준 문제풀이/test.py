4ec9083764c2617e1a77d499f01ac36dea8daa9b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-06-23 19:40:57 +0900

    directory change

--- 백준 문제풀이/test.py
+++ 백준 문제풀이/test.py
@@ -1,2 +1,10 @@
-for i in range(11, 0, -1):
-    print(i)
+# https://www.acmicpc.net/problem/2522
+
+n = int(input())
+
+for i in range(n, 0, -1):
+    print(" "*(i-1), end='')
+    print("*"*(n-(i-1)))
+for i in range(n-1, 0, -1):
+    print(" "*(n-i), end='')
+    print("*"*i)
