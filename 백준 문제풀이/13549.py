8d18889019aba4e168f0b0abe46a481af828496e
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-06-28 14:00:47 +0900

    백준 135549번 재풀이 : bfs, 간단하게 생각하기

--- 백준 문제풀이/13549.py
+++ 백준 문제풀이/13549.py
@@ -3,7 +3,6 @@
 출발지 n과 목적지 k가 주어졌을때 k에 도달하는데 걸리는 시간을 측정
 n에서 n-1과 n+1로 이동하는데는 각각 1초가 걸리지만
 2*n으로 이동하는데는 0초의 시간이 걸린다
-=> 아이디어 오류 폐기, 그냥 bfs로 수정
 '''
 from collections import deque
 
@@ -11,29 +10,29 @@ n,k = map(int,input().split())
 
 # 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다 => 최대 배열의 크기 설정
 graph = [-1 for _ in range(100001)]
-visited = [False for _ in range(100001)]
+
+dx = [2,-1,1]
 
 # 시작위치 n은 방문처리하고 목적지 k에 도달할때까지 bfs를 수행한다
 def bfs():
-    visited[n] = True
-    graph[n] = 0
     queue = deque()
     queue.append(n)
+    graph[n] = 0
     while queue:
         x = queue.popleft()
         # 목적지에 도달하였다면 종료
         if x==k:
             return graph[x]
-        # 좌우로 이동하거나, 순간이동하거나
-        for nx in (x + 1, x - 1, x * 2):
-            # 범위를 벗어나지 않으면서 방문하지 않은 점에 대하여
-            if 0<=nx<100001 and not visited[nx]:
-                if nx == x*2: # 순간이동이라면
-                    graph[nx] = graph[x] # 시간 누적이 없다(0초)
-                    queue.appendleft(nx) # 순간이동을 먼저 탐색해야한다.
-                else: # 만약 좌우로 이동하는 움직임이라면
-                    graph[nx] = graph[x] + 1 # 1초를 누적시키고
-                    queue.append(nx) # 큐에 삽입
-                visited[nx] = True # 방문처리 후
-    
+        # 좌우로 이동하는 경우만 생각
+        for i in range(3):
+            if i==0: # 순간이동의 경우
+                nx = x * dx[i]
+                if 0 <= nx <= 100001 and graph[nx]==-1:
+                    queue.append(nx) 
+                    graph[nx] = graph[x] # 순간이동은 시간소요x
+            else:
+                nx = x + dx[i]
+                if 0<=nx<100001 and graph[nx]==-1:
+                    queue.append(nx)
+                    graph[nx] = graph[x] + 1 # 걸어가는 경우 1초 소요        
 print(bfs())
