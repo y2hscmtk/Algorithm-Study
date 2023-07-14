82d2d0729751a864811b6309ec0f25056730eebf
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-07-14 17:38:44 +0900

    백준 17406번 배열 돌리기 4 푸는중 : 테두리 회전 구현

---
+++ 백준 문제풀이/17406.py
@@ -0,0 +1,65 @@
+# https://www.acmicpc.net/problem/17406
+import sys
+from itertools import permutations
+# 시간초과 날것같은데..
+
+N, M, K = map(int, input().split())
+
+# 배열의 모습
+A = [list(map(int, input().split())) for _ in range(N)]
+
+# 시행 가능한 연산의 수
+op = [list(map(int, input().split())) for _ in range(K)]
+
+
+result = sys.maxsize
+
+# def check():
+#     for t in temp_a:
+#         print(*t)
+
+# 위에서부터 시계방향으로 이동
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+# 배열 회전 함수
+def rotate(r, c, s):
+    # 임시 배열 생성
+    temp_a = [a[:] for a in A]
+
+    # 가장 왼쪽 윗 칸이 (r-s, c-s), 가장 오른쪽 아랫 칸이 (r+s, c+s)인 정사각형
+    # 가장 바깥쪽 테두리부터, 안쪽 테두리까지 좁혀가기 => 테투리를 줄이면 될듯
+    s_r, s_c = r-s-1, c-s-1
+    e_r, e_c = r+s-1, c+s-1
+    direction = 0
+    before = temp_a[s_r][s_c]
+    # 왼쪽 끝 좌표 설정
+    x, y = s_r, s_c+1
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        # 처음으로 되돌아 왔다면=>한바퀴 돌았다면 탈출
+        if x == s_r and y == s_c:
+            temp_a[x][y] = before
+
+            print("good")
+            break
+        # 각 테두리에서, 끝에 도달했다면 방향변경
+        if nx < s_r or nx > e_r or ny < s_c or ny > e_c:
+            direction += 1  # 방향 바꾸고
+            continue
+        # 오른쪽으로 한칸 이동
+        temp_a[x][y], before = before, temp_a[x][y]
+        # 이동 처리
+        x = nx
+        y = ny
+
+
+# 배열의 값 파악하는 함수
+def update_min():
+    for a in A:
+        result = min(result, sum(a))
+
+
+rotate(3, 4, 2)
