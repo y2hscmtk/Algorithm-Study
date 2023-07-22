dc34d62981d982b2acb93598a3359f8ada34f258
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-07-22 18:10:59 +0900

    백준 4470번 풀이

---
+++ 백준 문제풀이/4470.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/4470
+'''
+5
+Lionel Cosgrove
+Alice
+Columbus and Tallahassee
+Shaun and Ed
+Fido
+
+1. Lionel Cosgrove
+2. Alice
+3. Columbus and Tallahassee
+4. Shaun and Ed
+5. Fido
+'''
+for i in range(1, int(input())+1):
+    data = input()
+    print(f'{i}. '+data)
