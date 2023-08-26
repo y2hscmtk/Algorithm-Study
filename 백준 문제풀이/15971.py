db5f114f66422706ce611f243591084f30a42b70
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-08-26 22:28:26 +0900

    백준 15971번 두 로봇 풀이 : bfs,36점

--- 백준 문제풀이/15971.py
+++ 백준 문제풀이/15971.py
@@ -1,10 +1,10 @@
 # https://www.acmicpc.net/problem/15971
 '''
-두 정점 사이의 최단 경로를 기록하고 => 다익스트라
+두 정점 사이의 최단 경로를 기록하고 => 다익스트라 => bfs
 가장 긴 거리를 제외한 나머지 거리를 더해서 출력
 '''
-import heapq
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 N, start, end = map(int, input().split())
@@ -18,28 +18,20 @@ for _ in range(N-1):
     graph[b].append((a, c))
 
 
-def dijkstra():
-    INF = int(1e9)
-    queue = []
-    distance = [[INF, -1] for _ in range(N+1)]
-    # distance배열 값 초기화
-    for node, dist in graph[start]:
-        distance[node][0] = dist
-        distance[node][1] = dist
-        heapq.heappush(queue, (dist, node))
-    distance[start][0] = 0  # 자기 자신으로의 최단거리는 0
+def bfs():
+    queue = deque()
+    queue.append([start, 0, 0])
+    visited = [-1]*(N+1)
+    visited[start] = 0  # 시작 좌표까지의 거리는 0
     while queue:
-        curr_d, curr_node = heapq.heappop(queue)
+        curr_node, curr_d, max_length = queue.popleft()
+        if curr_node == end:  # 목적지 도달시
+            print(visited[end]-max_length)
         for node, dist in graph[curr_node]:
-            # curr_node를 거쳐갈 경우,
-            # node로의 최단거리가 갱신된다면 거쳐가기
-            # curr_d는 curr_node까지의 최단거리
-            if curr_d+dist < distance[node][0]:
-                distance[node][0] = curr_d+dist
-                distance[node][1] = max(distance[node][1], dist)
-                heapq.heappush(queue, (distance[node][0], node))
-    return distance
+            if visited[node] == -1:
+                visited[node] = curr_d + dist
+                max_length = max(max_length, dist)
+                queue.append([node, visited[node], max_length])
 
 
-distance = dijkstra()
-print(distance[end][0] - distance[end][1])
+bfs()
