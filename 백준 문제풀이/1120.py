137f191c47c7c3e5b2e526b88744e0294483a42c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-09-28 20:45:12 +0900

    1120

---
+++ 백준 문제풀이/1120.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/1120
+
+'''
+A의 앞에 아무 알파벳이나 추가한다.
+A의 뒤에 아무 알파벳이나 추가한다.
+'''
+import sys
+a,b = input().split()
+
+alphabet = "abcdefghijklnmopqrstuvwxyz"
+result = sys.maxsize
+def dfs(a):
+    global result
+    # 길이가 같아지기 전까지 연산 수행
+    if len(a)==len(b):
+        # 두 문자열 간의 차이가 몇인지 계산하여 갱신
+        count = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                count+=1 # 불일치 글자 +1
+        # 정답 갱신
+        result = min(result,count)
+        return
+    # 앞에 붙이거나
+    for i in alphabet:
+        dfs(a+i) # 뒤에 붙이거나
+        dfs(i+a) # 앞에 붙이거나
+
+dfs(a)
+print(result)
+    
--- 백준 문제풀이/test.py
+++ 백준 문제풀이/test.py
@@ -1,8 +1,6 @@
-data = [list(map(int, input().split())) for _ in range(9)]
-x, y, max_num = 0, 0, data[0][0]
-for i in range(9):
-    for j in range(9):
-        if data[i][j] > max_num:
-            x, y, max_num = i, j, data[i][j]
-print(max_num)
-print(i+1, j+1)
+test = ['사과','홍옥','아카네']
+
+def prompt(result,test):
+    print(result + str(test))
+
+prompt("테스트",test)
