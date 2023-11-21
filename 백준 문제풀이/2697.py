ff38bb9ba3ee4fe88e6c24d5e7d10773e848069b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-21 16:40:56 +0900

    백준 2697번 다음 수 구하기 풀이 : 그리디

--- 백준 문제풀이/2697.py
+++ 백준 문제풀이/2697.py
@@ -25,3 +25,37 @@
 # 4 이하의 수 오름차순으로 재정렬
 # ...423799
 # 정답: 279134423799
+
+# 모든 테스트 케이스에서 위 과정 수행
+for _ in range(int(input())):
+    number = list(map(int,input()))
+    # BIGGEEST검사
+    # 1의 자리부터 검사
+    # 321 => 1<=2<=3 => BIGGEST
+    isBiggest = True # biggest검사
+    for i in range(len(number)-1,0,-1):
+        if number[i] <= number[i-1]:
+            continue
+        isBiggest = False # biggest가 아닌 경우
+        # 123 => 3,2,1 => 조건수는 2가 되어야 함
+        index = i-1 # '조건수' 탐색
+        condition_number = number[i-1]
+        break # 그렇지 않은 경우 발견시
+    if isBiggest:
+        print("BIGGEST")
+        continue # 다음 테스트 케이스로 넘어가기
+    # IF NOT BIGGEST
+    # index의 오른쪽 수들 중에서 조건수보다 큰 수들 중 가장 작은 수 판별
+    select_index = index + 1 # 바로 오른쪽 수를 선택했다고 가정 => 어차피 한 개 이상 얻어걸릴것
+    for i in range(index+1,len(number)):
+        if condition_number < number[i]: # 조건수 보다 큰 경우
+            # 더 작거나 같은 수 발견 시(같은 경우, 더 작은 자리에서 바꾸는 것이 이득)
+            if number[i] <= number[select_index]:
+                select_index = i # 위치 저장
+    # 스왑
+    number[index],number[select_index] = number[select_index],number[index]
+    # index이후 오름차순 정렬
+    new_number = number[:index+1] + sorted(number[index+1:])
+    for n in new_number:
+        print(n,end='')
+    print()
