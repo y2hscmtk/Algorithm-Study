52cf14ad6fda5eef9e692dd19b9ba91990e20ced
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-24 16:05:39 +0900

    백준 10934번 이전 순열 풀이 : 수학,구현,그리디

--- 백준 문제풀이/10973.py
+++ 백준 문제풀이/10973.py
@@ -1,20 +1,36 @@
 # https://www.acmicpc.net/problem/10973
-from itertools import permutations
 '''
-만들 수 있는 모든 경우의 수를 배열에 저장 후 정렬
-배열에 저장된 수를 돌다가 주어진 배열과 같은 수 발견시, 이전 수 출력
-사전순으로 가장 처음에 오는 순열인 경우에는 -1을 출력한다.
+-1을 출력하게 되는 상황 => 모든 수가 내림차순인 상황
+모든 수가 내림차순이 아니라면? => '이전 수'를 만들 수 있다.
+문제의 접근 방법은 현재 수의 '이전 수'를 바로 찾는 것 => 조합을 사용하면 시간초과에 걸린다. 10000_P_10000 이므로..
+
 '''
+import sys
 n = int(input())
-numbers = [i for i in range(1, n+1)]
-array = list(map(int, input().split()))
-# 이전수 기록용 배열 last => 초기 값은 가장 처음 값으로 지정
-last = [*numbers]
-# 만들 수 있는 모든 경우의 수에 대해
-for i, a in enumerate(permutations(numbers, n)):
-    # permutations에 의해 결과물은 오름차순으로 정렬됨
-    if list(a) == array:  # 찾고자 하는 배열과 동일한 값 발견시
-        # 사전순으로 가장 처음에 오는 순열인지 확인(인덱스가 0인지 확인)
-        print(-1) if i == 0 else print(*last)
+current = list(map(int, input().split()))  # 현재 수 입력받기
+
+# 이미 가장 작은 수인지 확인
+array = [i for i in range(1, n+1)]
+if current == array:
+    print(-1)
+    sys.exit(0)
+
+# 뒤에서부터 오름차순을 만족하는 구간 확인
+for i in range(n-1, -1, -1):
+    # 5,3,1,2,4 => 3 > 1 이므로, 3 이하의 수에서는 오름차순
+    if current[i-1] > current[i]:
+        target = i-1
         break
-    last = [*a]  # 현재 값 저장
+
+
+# 타겟 이후의 수들 중에서 타겟보다 작은 수 중에서 가장 큰 수 찾기
+# 뒤의 배열은 이미 오름차순 이므로, 뒤에서부터 찾으면 조건을 만족하는 가장 큰 수를 찾을 수 있다.
+for i in range(n-1, target, -1):
+    if current[i] < current[target]:
+        index = i  # 인덱스 저장
+        break
+
+# 타겟 수와 자리 바꾼 후, 이후의 수는 내림차순으로 정렬(이전 수)
+current[target], current[index] = current[index], current[target]
+result = current[:target+1] + sorted(current[target+1:n], reverse=True)
+print(*result)
