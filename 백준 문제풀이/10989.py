0979b7d4f160ae364c665bae461bd16c007fcb49
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-02-13 18:56:41 +0900

    백준 19236번 청소년 상어 풀이 : 구현,시뮬레이션,백트래킹

--- 백준 문제풀이/10989.py
+++ 백준 문제풀이/10989.py
@@ -1,18 +1,13 @@
 # https://www.acmicpc.net/problem/10989
-
-# n개의 줄에 주어진 n개의 수를 정렬하여 한줄에 하나씩 출력
-
 import sys  # readline()은 더 적은 메모리를 사용한다?
-
-n = int(sys.stdin.readline())
-
+input = sys.stdin.readline
 # 메모리 관리를 해야하므로, 매번 배열을 생성하면 메모리 초과가 발생한다.
 # 따라서 기존에 미리 최대 입력한계까지 배열을 만들어 둔다.
 data = [0] * 10001
 
 # 입력받기
-for _ in range(n):
-    data[int(sys.stdin.readline())] += 1 # 입력받은 숫자의 인덱스 만큼 늘려서 표현한다.
+for _ in range(int(input())):
+    data[int(input())] += 1 # 입력받은 숫자의 인덱스 만큼 늘려서 표현
 # => 위과정을 통해 정렬까지 한번에 처리가 가능하다.
 
 for i in range(10001):
