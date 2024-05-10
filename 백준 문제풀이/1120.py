dddd7ca80efd98c80b442e97b38112094f95a62d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-11 01:34:03 +0900

    백준 1120번 문자열 풀이 : 문자열, 브루트포스

--- 백준 문제풀이/1120.py
+++ 백준 문제풀이/1120.py
@@ -1,32 +1,29 @@
 # https://www.acmicpc.net/problem/1120
-
 '''
-A의 앞에 아무 알파벳이나 추가한다.
-A의 뒤에 아무 알파벳이나 추가한다.
+1. A와 B의 길이가 같을 경우
+    - A와 B가 일치하는 문자의 개수를 파악한다.
+2. A와 B의 길이가 다를 경우
+    - A가 B보다 짧으므로 B에서 A와 가장 많이 겹치는 위치를 찾고, 
+        A의 앞뒤에 B와 일치하는 문자를 붙였다 가정한다.(A의 앞뒤에는 임의 문자 삽입 가능)
+
+A와 B의 길이가 같으면서, A와 B의 차이를 최소가 되도록 했을 때, 그 차이를 출력하시오.
 '''
 import sys
-a, b = input().split()
-
-result = sys.maxsize
-
-
-def dfs(a):
-    global result
-    # 길이가 같아지기 전까지 연산 수행
-    if len(a) == len(b):
-        # 두 문자열 간의 차이가 몇인지 계산하여 갱신
-        count = 0
-        for i in range(len(a)):
-            if a[i] != b[i]:
-                count += 1  # 불일치 글자 +1
-        # 정답 갱신
-        result = min(result, count)
-        return
-    # 앞에 붙이거나
-    for i in b:  # 모든 알파벳을 붙일필요없음 b에 있는것들만..
-        dfs(a+i)  # 뒤에 붙이거나
-        dfs(i+a)  # 앞에 붙이거나
-
-
-dfs(a)
-print(result)
+A, B = input().split()
+result = 0
+if len(A) == len(B):
+    for i in range(len(A)):
+        if A[i] != B[i]:
+            result += 1
+    print(result)
+else: # 길이가 다를 경우 A의 시작점을 어디로 설정해야 가장 많이 겹치는지 찾는다.
+    min_diff = sys.maxsize # 최소 차이
+    for start in range(0,len(B)-len(A)+1): # A를 놓을 위치 설정
+        t = 0 # A의 임시 인덱스
+        count = 0 # 차이값 갱신용
+        for i in range(start,start+len(A)): # A의 길이만큼
+            if A[t] != B[i]:
+                count += 1
+            t+=1
+        min_diff = min(min_diff,count)
+    print(min_diff)
