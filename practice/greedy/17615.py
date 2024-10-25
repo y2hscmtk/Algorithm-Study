ba2266f4dca8a08836666a2bffc9933946c71b4c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-25 21:36:33 +0900

    백준 17615번 볼 모으기 (두번째 시도) 15점 : 그리디

--- practice/greedy/17615.py
+++ practice/greedy/17615.py
@@ -3,39 +3,27 @@
 한번에 옮길 수 있는 공의 개수는 1개
 같은색끼리 만드는 것이 목표 
 -> 빨강색이든 파랑색이든 묶여있든 하나이든 결국 한쪽 끝으로 한 색을 모으는 것이 목표
+<15점>
+파란색공, 빨간색 공 위치 분리 후 각 공에 대해서 한쪽 끝으로 이동하기 각각 수행 이후 최소값 출력
+<15점>
+시간초과 판정을 줄이기 위해 공을 구분하면서 이동하는 과정을 한번에 수행
+-> 각 색깔 공이 이어져있는 수 카운팅(마지막까지 이어져있는 경우는 무시(이동할 필요x))
+-> 시간 단축 성공, 여전히 15점
 '''
 N = int(input())
 balls = input()
-# 빨간색 공, 파란색 공 연결된 위치 각각 입력받기
-last = balls[0]; group = [0,0] # 공 시작 위치
-red_ball = []; blue_ball = []
-for i in range(N):
-    ball = balls[i]
-    if ball == last: # 이전과 같은 색 공이라면 -> 이어져 있다면
-        group[1] = i # 공 위치 저장
-        if i == N-1:
-            red_ball.append(group) if last == 'R' else blue_ball.append(group)
-    else: # 이전과 다른색 공이라면 -> 연결이 끊어짐
-        red_ball.append(group) if last == 'R' else blue_ball.append(group)
-        last = ball
-        group = [i,i] # 그룹 초기화
-
-# 빨간색 공에 대해서
-red_count = 0
-for x,y in red_ball:
-    # 이미 끝자리에 위치하는 경우는 공을 이동할 필요 없음
-    if y == N-1:
-        continue
-    # 공은 한번에 한개씩 이동 가능
-    # for i in range(x,y+1):
-    #     red_count += 1
-    red_count += (y-x+1) # 각각의 공을 모두 옮길 것이므로
-
-# 파란색 공에 대해서
-blue_count = 0
-for x,y in blue_ball:
-    if y == N-1:
-        continue
-    blue_count += (y-x+1)
+red_count = 0; blue_count = 0 # 각 색깔공 최소 이동 횟수
+last = balls[0] # 시작 공의 색상
+count = 1
+for i in range(1,N): # 두번째 공부터 끝 공까지 비교 시작
+    if balls[i] == last:
+        count += 1
+    else: # 다른 색인 경우 -> 공이 분리된 경우
+        if last == 'R':
+            red_count += count # 지금까지 기록한 수 저장
+        else:
+            blue_count += count
+        count = 1 # 현재 공에서부터 다시 카운팅
+        last = balls[i] # 현재 색상의 공으로 변경
 
 print(min(red_count,blue_count))
