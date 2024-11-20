59e76ace7c86fc9e4ddba4c81ccdbf4f080492e7
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-11-21 00:03:10 +0900

    SWEA 2805번 농작물 수확하기 풀이 : 구현

---
+++ SW Expert Academy/2805.py
@@ -0,0 +1,21 @@
+'''
+# 1. 조건 찾아서 반복문 분할하여 해결
+# 2. 조건 찾아서 변수의 값을 바꿔가며 반복문에 적용
+'''
+for tc in range(1,int(input())+1):
+    N = int(input())
+    result = 0
+    board = [list(map(int,input().strip())) for _ in range(N)]
+    
+    # 합을 더하기 위한 각 행의 s,e영역의 값 변경시키기
+    M = N//2
+    s = e = M
+    for i in range(N):
+        for j in range(s,e+1):
+            result += board[i][j]
+        if i < M:
+            s-=1; e+=1
+        else:
+            s+=1; e-=1
+    
+    print(f'{tc} {result}')
