35aaf82a0ccc2692c3cce0d2daa9b3dd268e0f15
y2hscmtk <y2_12@naver.com>
2025-01-10 23:28:33 +0900

    백준 15655번 N과 M (10) 풀이 : 백트래킹

---
+++ practice/back-tracking/15665.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/15665
+'''
+N개의 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+'''
+n,m = map(int,input().split())
+numbers = sorted(list(map(int,input().split()))) # 사전순 출력을 위해 미리 정렬
+
+select = [0]*m # 선택할 수
+result = set()
+def make_array(idx):
+    if idx == m:
+        result.add(tuple(select))
+        return
+    
+    # 0번째 인덱스부터 순차적으로 확인
+    for i in range(n):
+        select[idx] = numbers[i]
+        make_array(idx+1) 
+        select[idx] = 0 # 사용 취소 처리 - 백트래킹
+
+make_array(0)
+
+# 정답 출력
+result = sorted(list(result))
+for select in result:
+    print(*select)
