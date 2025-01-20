38a72c5fff32ff01b4fc78dffb0170e34c02a8db
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-01-20 20:29:35 +0900

    코드트리 기울어진 사각형 풀이(업데이트) : 구현

--- practice/code-tree/implementation/기울어진 사각형.py
+++ practice/code-tree/implementation/기울어진 사각형.py
@@ -7,7 +7,7 @@
 2. 탐색 시작할 위치 정하기 (n*n)
 3<=n<=20
 
-O(n^4) = 400*400 = O(160000)
+O(n^5)
 
 '''
 n = int(input())
@@ -18,48 +18,32 @@ grid = [list(map(int,input().split())) for _ in range(n)]
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-
 # 탐색
 # 탐색 과정중 영역을 벗어나는 경우는 고려 x
-def search(x,y):
+def search(x,y,k,l):
     # 1번 경로로 이동시 - x-1,y+1 # 2번 경로로 이동시 - x-1,y-1
     # 3번 경로로 이동시 - x+1,y-1 # 4번 경로로 이동시 - x+1,y+1
-    dxs = [-1,-1,1,1]; dys = [1,-1,-1,1]
-    temp_max_sum = 0
-    for px in range(1,n+1):
-        for py in range(1,n+1):
-            c_x,c_y = x,y
-            fail = False
-            temp_sum = 0
-            # 이동할 방향 설정
-            for dx,dy in zip(dxs,dys):
-                if fail: # 설정한 크기만큼 이동하는것이 실패한 경우
-                    break
-                if (dx,dy) == (-1,1) or (dx,dy) == (1,-1):
-                    for _ in range(px):
-                        nx,ny = c_x + dx, c_y + dy
-                        if not in_range(nx,ny):
-                            fail = True
-                            break
-                        temp_sum += grid[nx][ny]
-                        c_x,c_y = nx,ny
-                else:
-                    for _ in range(py):
-                        nx,ny = c_x + dx, c_y + dy
-                        if not in_range(nx,ny):
-                            fail = True
-                            break
-                        temp_sum += grid[nx][ny]
-                        c_x,c_y = nx,ny
-            # 실패하지 않았다면
-            if not fail:
-                temp_max_sum = max(temp_max_sum, temp_sum)
-    return temp_max_sum                
+    dxs = [-1,-1,1,1]
+    dys = [1,-1,-1,1]
+    move_num = [k,l,k,l]
+    temp_sum = 0
+    # k,l은 각 대각선의 방향에서 이동해야할 칸의 개수
+    for dx,dy,num in zip(dxs,dys,move_num):
+        for _ in range(num): # 현재 방향에서의 num만큼 이동
+            nx,ny = x + dx, y + dy
+            if not in_range(nx,ny):
+                return 0 # 현재 수행에서 실패하는 경우
+            temp_sum += grid[nx][ny]
+            x,y = nx,ny
+    return temp_sum
 
 max_sum = 0 # 최대 숫자들의 합
 # 탐색 시작 위치 지정
 for i in range(n):
     for j in range(n):
-        max_sum = max(max_sum, search(i,j))
+        # 각 대각선에서 이동할 길이 설정
+        for k in range(1,n):
+            for l in range(1,n):
+                max_sum = max(max_sum, search(i,j,k,l))
                 
 print(max_sum)
